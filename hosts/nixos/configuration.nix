e201b1ab64bf22773f91848e51508fc911c1571b
Andrew Gluck <andrew.gluck91@gmail.com>
2024-11-04 10:36:52 -0500

    clean up

--- hosts/nixos/configuration.nix
+++ hosts/nixos/configuration.nix
@@ -1,6 +1,3 @@
-# Edit this configuration file to define what should be installed on
-# your system.  Help is available in the configuration.nix(5) man page
-# and in the NixOS manual (accessible by running ‘nixos-help’).
 {
   pkgs,
   inputs,
@@ -16,37 +13,28 @@
     ../../modules/nixos/hyprland.nix
   ];
 
-  # Bootloader.
-  boot.loader.grub.enable = true;
-  boot.loader.grub.device = "/dev/vda";
-  boot.loader.grub.useOSProber = true;
-
-  networking.hostName = "nix-dev"; # Define your hostname.
-  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
-
-  # Enable networking
-  networking.networkmanager.enable = true;
-
-  # Enable flatpak
-  services.flatpak.enable = true;
-
-  services.xserver = {
-    # Enable the X11 windowing system.
+  boot.loader.grub = {
     enable = true;
+    device = "/dev/vda";
+    useOSProber = true;
+  };
 
-    # Configure keymap in X11
-    xkb = {
-      layout = "us";
-      variant = "";
-    };
+  networking = {
+    hostName = "nix-dev";
+    networkmanager.enable = true;
+  };
 
-    # GPU drivers
-    videoDrivers = ["modesetting"];
-
-    # Enable the GNOME Desktop Environment.
-    displayManager.gdm.enable = true;
-    desktopManager = {
-      gnome = {
+  services = {
+    flatpak.enable = true;
+    xserver = {
+      enable = true;
+      xkb = {
+        layout = "us";
+        variant = "";
+      };
+      videoDrivers = ["modesetting"];
+      displayManager.gdm.enable = true;
+      desktopManager.gnome = {
         enable = true;
         extraGSettingsOverridePackages = [pkgs.mutter];
         extraGSettingsOverrides = ''
@@ -55,76 +43,49 @@
         '';
       };
     };
+    printing.enable = true;
+    qemuGuest.enable = true;
+    openssh.enable = true;
   };
-
-  programs.dconf.enable = true;
-
-  # Enable CUPS to print documents.
-  services.printing.enable = true;
-  services.qemuGuest.enable = true;
 
   security.rtkit.enable = true;
 
-  # Enable the Flakes feature and the accompanying new nix command-line tool
   nix.settings.experimental-features = ["nix-command" "flakes"];
 
-  # sets ZSH has default shell
-  programs.zsh.enable = true;
-  users.defaultUserShell = pkgs.zsh;
-
-  # Define a user account. Don't forget to set a password with ‘passwd’.
-  users.users.agluck = {
-    isNormalUser = true;
-    description = "agluck";
-    extraGroups = ["networkmanager" "wheel" "audio"];
+  programs = {
+    dconf.enable = true;
+    zsh.enable = true;
+    firefox.enable = true;
   };
 
-  programs.firefox.enable = true;
+  users = {
+    defaultUserShell = pkgs.zsh;
+    users.agluck = {
+      isNormalUser = true;
+      description = "agluck";
+      extraGroups = ["networkmanager" "wheel" "audio"];
+    };
+  };
+
   nixpkgs.config.allowUnfree = true;
 
-  # List packages installed in system profile.
-  environment.systemPackages = with pkgs; [
-    vim
-    wget
-    git
-    curl
-    nil
-    zip
-    unzip
-    cmake
-    gnumake
-    cargo
-    inputs.alejandra.defaultPackage.${pkgs.system}
-  ];
+  environment = {
+    systemPackages = with pkgs; [
+      vim
+      wget
+      git
+      curl
+      nil
+      zip
+      unzip
+      cmake
+      gnumake
+      cargo
+      inputs.alejandra.defaultPackage.${pkgs.system}
+    ];
+    pathsToLink = ["/share/zsh"];
+    shells = with pkgs; [zsh];
+  };
 
-  environment.pathsToLink = ["/share/zsh"];
-
-  environment.shells = with pkgs; [zsh];
-
-  # Some programs need SUID wrappers, can be configured further or are
-  # started in user sessions.
-  # programs.mtr.enable = true;
-  # programs.gnupg.agent = {
-  #   enable = true;
-  #   enableSSHSupport = true;
-  # };
-
-  # List services that you want to enable:
-
-  # Enable the OpenSSH daemon.
-  services.openssh.enable = true;
-
-  # Open ports in the firewall.
-  # networking.firewall.allowedTCPPorts = [ ... ];
-  # networking.firewall.allowedUDPPorts = [ ... ];
-  # Or disable the firewall altogether.
-  # networking.firewall.enable = false;
-
-  # This value determines the NixOS release from which the default
-  # settings for stateful data, like file locations and database versions
-  # on your system were taken. It‘s perfectly fine and recommended to leave
-  # this value at the release version of the first install of this system.
-  # Before changing this value read the documentation for this option
-  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
-  system.stateVersion = "24.05"; # Did you read the comment?
+  system.stateVersion = "24.05";
 }
