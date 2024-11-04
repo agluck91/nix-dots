b82b4b928a2c35044ac4ad3b6da3f44c5d8ebae5
Andrew Gluck <andrew.gluck91@gmail.com>
2024-11-04 14:14:12 -0500

    more

--- modules/nixos/gaming.nix
+++ modules/nixos/gaming.nix
@@ -8,32 +8,7 @@
   hardware.amdgpu.initrd.enable = true;
 
   programs.steam.enable = true;
-  programs.steam.gamescopeSession.enable = true;
   programs.gamemode.enable = true;
-
-  environment.systemPackages = with pkgs; [
-    protonup
-    amdgpu_top
-    lact
-    (lutris.override {
-      extraPkgs = pkgs: [
-        wineWowPackages.waylandFull
-        winetricks
-        gamemode
-      ];
-      extraLibraries = pkgs: [gamemode];
-    })
-  ];
-
-  systemd.services.lact = {
-    enable = true;
-    description = "AMDGPU Control Daemon";
-    after = ["multi-user.target"];
-    wantedBy = ["multi-user.target"];
-    serviceConfig = {
-      ExecStart = "${pkgs.lact}/bin/lact daemon";
-    };
-  };
 
   environment.sessionVariables = {
     STEAM_EXTRA_COMPAT_TOOLS_PATHS = "\${HOME}/.steam/root/compatibilitytools.d";
