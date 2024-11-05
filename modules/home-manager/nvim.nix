651d25425f4166ec4dc281eb2eb0882a476743a0
Andrew Gluck <andrew.gluck91@gmail.com>
2024-11-05 11:17:07 -0500

    try agin

--- modules/home-manager/nvim.nix
+++ modules/home-manager/nvim.nix
@@ -14,6 +14,7 @@
 
   programs.neovim = {
     enable = true;
+    package = inputs.neovim-nightly-overlay.packages.${pkgs.system}.default;
     defaultEditor = true;
     viAlias = true;
     vimAlias = true;
