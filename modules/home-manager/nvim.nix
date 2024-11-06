89b1746f4ea3bcfd99a7e1b2f61fa0f2b6653588
Andrew Gluck <andrew.gluck91@gmail.com>
2024-11-06 16:34:50 -0500

    asdf

--- modules/home-manager/nvim.nix
+++ modules/home-manager/nvim.nix
@@ -148,10 +148,6 @@
           config = lib.fileContents ../../config/nvim/plugins/img-clip.lua;
         }
         {
-          plugin = avante;
-          config = lib.fileContents ../../config/nvim/plugins/avante.lua;
-        }
-        {
           plugin = cloak-nvim;
           config = lib.fileContents ../../config/nvim/plugins/cloak.lua;
         }
