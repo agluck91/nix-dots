5f3937b9816517468165952fadb4254858666013
Andrew Gluck <andrew.gluck@prounlimited.com>
2024-11-19 16:47:01 -0500

    local

--- hosts/mac/configuration.nix
+++ hosts/mac/configuration.nix
@@ -36,5 +36,11 @@ in {
   programs.zsh.enable = true;
   environment.pathsToLink = ["/share/zsh"];
   environment.shells = with pkgs; [zsh];
-  programs.bash.enable = false;
+  environment.variables = {
+    LANG = "en_US.UTF-8";
+    LC_ALL = "en_US.UTF-8";
+    locale = "en_US.UTF-8";
+    XDG_CONFIG_PATH = "/home/${username}/.config/";
+    TERM = "xterm-256color";
+  };
 }
--- hosts/mac/home.nix
+++ hosts/mac/home.nix
@@ -5,6 +5,7 @@
     ../../modules/home-manager/volta.nix
     ../../modules/home-manager/nvim.nix
     ../../modules/home-manager/apps.nix
+    ../../modules/home-manager/zsh.nix
   ];
 
   home.stateVersion = "24.05";
