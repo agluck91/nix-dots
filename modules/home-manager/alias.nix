d87d3b591a33de7c7f50debba869201cc586ca8f
Andrew Gluck <andrew.gluck91@gmail.com>
2024-11-21 15:20:12 -0500

    add

--- modules/home-manager/alias.nix
+++ modules/home-manager/alias.nix
@@ -1,10 +1,10 @@
 {
   #nix Aliases
-
   nix-r = "sudo nixos-rebuild switch --flake ~/nix-dots#nixos";
+  nix-mr = "darwin-rebuild switch --flake ~/nix-dots#mac";
   nix-u = "sudo flakes update";
   nix-e = "nvim ~/nix-dots";
-  nix-g = "nix collect-garbage -d";
+  nix-g = "sudo nix collect-garbage -d 1w";
 
   #System Aliases
   ls = "exa --icons -gh --group-directories-first";
@@ -12,6 +12,8 @@
   la = "exa --icons -gh --group-directories-first -a";
   lla = "exa --icons -gh --group-directories-first -la";
   llt = "exa --icons --group-directories-first -T";
+  cd = "z";
+  cdi = "zi";
   cc = "/bin/cat";
   cat = "bat -p -P";
   rm = "rm -i";
@@ -19,11 +21,9 @@
   mv = "mv -i";
   zshrc = "nvim ~/.zshrc";
   tf = "tail -f -n 1000";
-  h = "history";
 
   #Misc aliases
   lg = "lazygit";
-  vim = "nvim";
 
   #Docker aliases
   drm = "docker rm -f $(docker ps -aq)";
@@ -32,11 +32,36 @@
   dnuke = "drm; drmi; docker system prune -a --volumes --force";
   d = "docker";
   dc = "docker compose";
-  dcu = "docker compose up";
+  dcu = "docker compose up -d";
   dcd = "docker compose down";
-  dcud = "docker compose up -d";
   dr = "docker run";
   dex = "docker exec -it";
+
+  #Kubernetes aliases
+  k = "kubectl";
+  kg = "kubectl get";
+  kd = "kubectl describe";
+  krm = "kubectl delete";
+  kaf = "kubectl apply -f";
+  klo = "kubectl logs -f";
+  kex = "kubectl exec -it";
+  kpf = "kubectl port-forward";
+  kgi = "kubectl get ingress";
+  kgr = "kubectl get roles";
+  kga = "kubectl get rolebindings";
+  kgc = "kubectl get configmaps";
+  kgs = "kubectl get secrets";
+  kgn = "kubectl get nodes";
+  kdp = "kubectl describe pods";
+  kds = "kubectl describe services";
+  kdd = "kubectl describe deployments";
+  h = "helm";
+  hi = "helm install";
+  hu = "helm uninstall";
+  hl = "helm list";
+  hls = "helm ls";
+  hld = "helm delete";
+  hldp = "helm delete --purge";
 
   #Git Aliases
   g = "git";
