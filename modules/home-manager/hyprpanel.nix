eda25525348c6db5bf6499233bd50bd79043cb3a
Andrew Gluck <andrew.gluck91@gmail.com>
2025-01-14 15:14:04 -0500

    hyprpanel

--- modules/home-manager/hyprpanel.nix
+++ modules/home-manager/hyprpanel.nix
@@ -2,57 +2,22 @@
   imports = [inputs.hyprpanel.homeManagerModules.hyprpanel];
 
   programs.hyprpanel = {
-    overlay.enable = true;
-
-    # Enable the module.
-    # Default: false
     enable = true;
-
-    # Automatically restart HyprPanel with systemd.
-    # Useful when updating your config so that you
-    # don't need to manually restart it.
-    # Default: false
     systemd.enable = true;
-
-    # Add '/nix/store/.../hyprpanel' to your
-    # Hyprland config 'exec-once'.
-    # Default: false
-    hyprland.enable = true;
-
-    # Fix the overwrite issue with HyprPanel.
-    # See below for more information.
-    # Default: false
     overwrite.enable = true;
 
-    # Import a theme from './themes/*.json'.
-    # Default: ""
     theme = "catppuccin_mocha";
 
-    # Override the final config with an arbitrary set.
-    # Useful for overriding colors in your selected theme.
-    # Default: {}
-    # override = {
-    #   theme.bar.menus.text = "#123ABC";
-    # };
-
-    # Configure bar layouts for monitors.
-    # See 'https://hyprpanel.com/configuration/panel.html'.
-    # Default: null
     layout = {
       "bar.layouts" = {
-        "0" = {
-          left = ["dashboard" "workspaces"];
-          middle = ["media"];
-          right = ["volume" "notifications"];
+        "*" = {
+          left = ["dashboard" "media" "windowtitle"];
+          middle = ["workspaces"];
+          right = ["volume" "network" "bluetooth" "clock" "notifications"];
         };
       };
     };
 
-    # Configure and theme almost all options from the GUI.
-    # Options that require '{}' or '[]' are not yet implemented,
-    # except for the layout above.
-    # See 'https://hyprpanel.com/configuration/settings.html'.
-    # Default: <same as gui>
     settings = {
       bar.launcher.autoDetectIcon = true;
       bar.workspaces.show_icons = true;
@@ -65,7 +30,7 @@
         weather.unit = "metric";
       };
 
-      menus.dashboard.directories.enabled = true;
+      menus.dashboard.directories.enabled = false;
       menus.dashboard.stats.enable_gpu = true;
 
       theme.bar.transparent = true;
