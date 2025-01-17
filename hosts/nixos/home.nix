7a380101da700c17291b1b2122548a83f0e0c7f0
Andrew Gluck <andrew.gluck91@gmail.com>
2025-01-17 11:03:29 -0500

    defaults

--- hosts/nixos/home.nix
+++ hosts/nixos/home.nix
@@ -21,5 +21,17 @@
     stateVersion = "24.05";
   };
 
+  xdg.mimeApps = {
+    enable = true;
+
+    defaultApplications = {
+      "text/html" = "google-chrome.desktop";
+      "x-scheme-handler/http" = "google-chrome.desktop";
+      "x-scheme-handler/https" = "google-chrome.desktop";
+      "x-scheme-handler/about" = "google-chrome.desktop";
+      "x-scheme-handler/unknown" = "google-chrome.desktop";
+    };
+  };
+
   programs.home-manager.enable = true;
 }
